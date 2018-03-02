2f6a278aa63f21564dd1db3e4eedff5373e7a2e2
Owen Rasar <stephanierasar@yahoo.com>
2018-03-02 20:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function nowServing(katzDeli){
     return "There is nobody waiting to be served!";
   }
 }
+
+
+var line = [];
+
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
