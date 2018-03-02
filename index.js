2868af21fab0dd0f04799fe7411aabc81c2317c7
Owen Rasar <stephanierasar@yahoo.com>
2018-03-02 20:13:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function takeANumber(katzDeli, name){
+  katzDeli.push(`${name}`);
+return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  
+}
+
+function nowServing(katzDeli){
+ if (katzDeli.length > 0) {
+  return (`Currently serving ${katzDeli.shift()}.`)
+  
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
